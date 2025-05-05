1849aafde3483f2973df1d48a2d237c8eb18c1d1
AlvaroChS <alvaro.chillari.sanchez@alumnos.upm.es>
2025-05-05 20:00:27 +0200

    prueba final (salio bien)

--- prueba1/tablero.cpp
+++ prueba1/tablero.cpp
@@ -7,3 +7,4 @@
 //nuevo intento
 //creo que para que se pueda subir el archivo en github hay que cerrar el visual studio primero
 //prueba usando el archivo gitignore
+//prueba usando git para eliminar los archivos temporales que se han subido (no deberian subirse mas archivos que los cambios de codigo)
