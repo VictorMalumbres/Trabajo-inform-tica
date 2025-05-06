91431ebdce812f770a1fb4276f2b0c97c054e432
cristinabuisac <cristina.buisac@alumnos.upm.es>
2025-05-06 15:21:21 +0200

    preub

--- prueba1/tablero.cpp
+++ prueba1/tablero.cpp
@@ -8,3 +8,6 @@
 //creo que para que se pueda subir el archivo en github hay que cerrar el visual studio primero
 //prueba usando el archivo gitignore
 //prueba usando git para eliminar los archivos temporales que se han subido (no deberian subirse mas archivos que los cambios de codigo)
+
+
+//pruebaa
