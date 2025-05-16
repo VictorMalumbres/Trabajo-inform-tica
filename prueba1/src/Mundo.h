8c498eebd3a5033d79bfb7e1cff7cf692b180d91
cristinabuisac <cristina.buisac@alumnos.upm.es>
2025-05-16 16:19:30 +0200

    .

--- prueba1/src/Mundo.h
+++ prueba1/src/Mundo.h
@@ -1,14 +1,14 @@
 #pragma once
-#include "Tablero.h"
+#include "tablero.h"
 #include <freeglut.h>
 
 class Mundo {
 private:
-    Tablero tablero;  // Tablero
-    Tablero tablero2; //2do juego
+    Tablero tablero;  // Tablero 
+	Tablero tablero2; // Tablero 2
 
 public:
-    Mundo(); 
+    Mundo();
     void inicializa();  // Inicializar el mundo (tablero, piezas, etc.)
     void dibuja();  // Dibuja el tablero y las piezas
 };
