501ac3590f67dac15e5f0325e31f2197e5e638f8
pilarftorrejon <pilar.ftorrejon@alumnos.upm.es>
2025-05-28 11:51:33 +0200

    minion peon

--- prueba1/src/Peon.cpp
+++ prueba1/src/Peon.cpp
@@ -4,59 +4,37 @@
 #include "Tablero.h"
 
 void Peon::dibuja() {
-    if (resaltado)glColor3f(1.0f, 1.0f, 0.0f); // Amarillo para resaltado
-    
-    if (getBando() == 0)
-    {
-        // Dibujo del fondo con textura
-        glPushMatrix();  // Guardamos la matriz actual
+    glPushMatrix();
+    glTranslated(getX() + 0.5f, getY() + 0.5f, 0);
 
-        // Posicionamos la imagen centrada en la celda
-        glTranslated(getX() + 0.5f, getY() + 0.5f, 0);  // Centrado en la celda
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-        glEnable(GL_TEXTURE_2D);
-        glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/peon_blanco.png").id);
-        // Dibujamos un cuadrado centrado con tamaño 0.6x0.6 (como el cubo anterior)
-        float size = 0.3f;  // Mitad del tamaño total (0.6 / 2)
-        glBegin(GL_QUADS);
-        colorR = 1.0f;
-        colorG = 1.0f;
-        colorB = 1.0f;  // blanco
-        glColor3f(colorR, colorG, colorB);  // blanco
-        glTexCoord2d(0, 1); glVertex3f(-size, -size, 0);  // esquina inferior izquierda
-        glTexCoord2d(1, 1); glVertex3f(size, -size, 0);  // esquina inferior derecha
-        glTexCoord2d(1, 0); glVertex3f(size, size, 0);  // esquina superior derecha
-        glTexCoord2d(0, 0); glVertex3f(-size, size, 0);  // esquina superior izquierda
-        glEnd();
-        glDisable(GL_TEXTURE_2D);
-        glPopMatrix();  // Restauramos la matriz original
+    if (getBando() == 0) {
+        glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/minions.png").id);
     }
-    else
-    {
-        // Dibujo del fondo con textura
-        glPushMatrix();  // Guardamos la matriz actual
-
-        // Posicionamos la imagen centrada en la celda
-        glTranslated(getX() + 0.5f, getY() + 0.5f, 0);  // Centrado en la celda
-
-        glEnable(GL_TEXTURE_2D);
+    else {
         glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/peon_negro.png").id);
-        // Dibujamos un cuadrado centrado con tamaño 0.6x0.6 (como el cubo anterior)
-        float size = 0.3f;  // Mitad del tamaño total (0.6 / 2)
-        glBegin(GL_QUADS);
-        colorR = 1.0f;
-        colorG = 1.0f;
-        colorB = 1.0f; //negro
-        glColor3f(colorR, colorG, colorB);  // negro
-        glTexCoord2d(0, 1); glVertex3f(-size, -size, 0);  // esquina inferior izquierda
-        glTexCoord2d(1, 1); glVertex3f(size, -size, 0);  // esquina inferior derecha
-        glTexCoord2d(1, 0); glVertex3f(size, size, 0);  // esquina superior derecha
-        glTexCoord2d(0, 0); glVertex3f(-size, size, 0);  // esquina superior izquierda
-        glEnd();
-        glDisable(GL_TEXTURE_2D);
-        glPopMatrix();  // Restauramos la matriz original
     }
+
+    glColor3f(1.0f, 1.0f, 1.0f);  // Color blanco para no modificar la textura
+
+    float size = 0.3f;
+    glBegin(GL_QUADS);
+   glTexCoord2d(0, 1); glVertex3f(-size, -size, 0);
+glTexCoord2d(1, 1); glVertex3f(size, -size, 0);
+glTexCoord2d(1, 0); glVertex3f(size, size, 0);
+glTexCoord2d(0, 0); glVertex3f(-size, size, 0);
+
+    glEnd();
+
+    glDisable(GL_BLEND);
+    glDisable(GL_TEXTURE_2D);
+
+    glPopMatrix();
 }
+
 
 bool Peon::mueve(Tablero& tablero, int nuevaColumna, int nuevaFila) {
     int origenX = getX();
