3ba7b85b997602008cdbf94dd0a5d8a4e9150383
cristinabuisac <cristina.buisac@alumnos.upm.es>
2025-05-28 13:38:18 +0200

    ,

--- prueba1/src/Mundo.cpp
+++ prueba1/src/Mundo.cpp
@@ -105,7 +105,7 @@ void Mundo::mostrarMenuEnVentana() {
 
     // Fondo con textura
     glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/fondo_menu.png").id);
+    glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture("imagenes/MENU.png").id);
 
     glBegin(GL_QUADS);
     glTexCoord2f(0, 0); glVertex2f(-1.0f, -1.0f);
